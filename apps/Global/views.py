9c17745152ff5d67be89c53fcef97eaa7dd0da51
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-08 03:39:43 +0200

    Update views.py

--- apps/Global/views.py
+++ apps/Global/views.py
@@ -8,6 +8,14 @@ class HomePageView(TemplateView):
 
 class Error404View(TemplateView):
     template_name = 'Cliente/Error_404.html'
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            return super().dispatch(request, *args, **kwargs)
+        except Exception as e:
+            # Registra el error en el logger
+            logger.exception("Error 404: %s", e)
+            # Re-lanza la excepción para que Django maneje el error como de costumbre
+            raise
 
 class Error505View(TemplateView):
     template_name = 'Cliente/Error_404.html'
@@ -16,7 +24,13 @@ class Error505View(TemplateView):
     def as_error_view(cls):
         v = cls.as_view()
         def view(request):
-            r = v(request)
-            r.render()
-            return r
-        return view
+            try:
+                r = v(request)
+                r.render()
+                return r
+            except Exception as e:
+                # Registra el error en el logger
+                logger.exception("Error 505: %s", e)
+                # Re-lanza la excepción para que Django maneje el error como de costumbre
+                raise
+        return view 
