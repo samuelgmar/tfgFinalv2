0b3327c8eea01e52b0027ee8d5b545dc0fbbb8fb
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-09 02:43:41 +0200

    Update settings.py

--- LotoFinder/settings.py
+++ LotoFinder/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_DIRS =  os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'Global.Usuario'
 
