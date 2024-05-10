7a58d362d9450c7708f842a71312e79b4ed4bd3c
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-10 19:36:34 +0200

    Update urls.py

--- LotoFinder/urls.py
+++ LotoFinder/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     path("cookies/", include("cookie_consent.urls")),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = Error404View.as_view()
 handler500 = Error505View.as_error_view()
