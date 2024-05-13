efd65f760aa5209ea894e1de52b5b31ab44e0848
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-13 05:21:34 +0200

    Actualizar settings.py

--- LotoFinder/settings.py
+++ LotoFinder/settings.py
@@ -149,7 +149,7 @@ SESSION_COOKIE_AGE = 3600 * 12
 
 CART_SESSION_ID = 'cart'
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = "media"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
