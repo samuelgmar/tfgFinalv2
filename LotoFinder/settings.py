89eebffa8709e974314388d91068bcdc7155a594
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-13 04:27:11 +0200

    Actualizar settings.py

--- LotoFinder/settings.py
+++ LotoFinder/settings.py
@@ -152,7 +152,9 @@ CART_SESSION_ID = 'cart'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_HOST = "https://lotofinder.es" if not DEBUG else ""
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
