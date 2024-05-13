b36415fd275d068891de93162f8f1107bfdde108
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-13 04:18:34 +0200

    Actualizar settings.py

--- LotoFinder/settings.py
+++ LotoFinder/settings.py
@@ -153,6 +153,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_HOST = "https://lotofinder.es" if not DEBUG else ""
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / 'media']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
