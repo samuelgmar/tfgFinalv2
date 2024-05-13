ed30ae4996b4bb5b91eeb48fbcdc1cbbdfe7c61b
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-13 04:34:11 +0200

    Actualizar urls.py

--- LotoFinder/urls.py
+++ LotoFinder/urls.py
@@ -23,9 +23,10 @@ urlpatterns = [
     path('<str:nombre_administracion>/', include(('apps.Cliente.urls','LotoCliente'))),
     #cookies
     path("cookies/", include("cookie_consent.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 handler404 = Error404View.as_view()
