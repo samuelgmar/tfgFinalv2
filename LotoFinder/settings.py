948bfeb3a2e7119b2d79a0df412ba6f4503aad77
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2024-05-18 20:13:32 +0200

    Update settings.py

--- LotoFinder/settings.py
+++ LotoFinder/settings.py
@@ -124,6 +124,27 @@ USE_TZ = True
 
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        '__main__': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
   
 
